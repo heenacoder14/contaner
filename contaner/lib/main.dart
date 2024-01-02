65632f5034b3ab40fe6080153e3252829d91aeb7
heena <heenapraveen143006@gmail.com>
2024-01-02 11:34:31 +0530

    create file home page

--- contaner/lib/main.dart
+++ contaner/lib/main.dart
@@ -1,4 +1,5 @@
    import 'package:flutter/material.dart';
+    import 'homepage.dart';
          void main() {
    runApp(const MyApp());
 }
@@ -17,51 +18,3 @@
     }
   }
 
-  class HomePage extends StatefulWidget {
-    const HomePage({super.key});
-    @override
-    State<HomePage> createState() => _HomePageState();
-  }
-
-  class _HomePageState extends State<HomePage> {
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("FOME",style: TextStyle(fontSize: 30,color: Colors.black))),
-        ),
-
-        body:Column(
-          children: [
-            SizedBox(
-              height:50 ,
-            ),
-            Container(
-
-              child: TextField(
-                decoration: InputDecoration(hintText: "enter name",border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-
-              child: TextField(
-                decoration: InputDecoration(hintText: "enter email",border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-
-              child: TextField(
-                decoration: InputDecoration(hintText: "enter email pasword",border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
-              ),
-            ),
-          ],
-        ),
-      );
-}
-  }
