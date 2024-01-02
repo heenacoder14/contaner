9400df5947b8165c71e17dc607a3e4d1085aa13b
heena <heenapraveen143006@gmail.com>
2024-01-02 12:05:52 +0530

    buttom cereate and coding

--- contaner/lib/homepage.dart
+++ contaner/lib/homepage.dart
@@ -21,8 +21,12 @@ class _HomePageState extends State<HomePage> {
           ),
           Container(
 
-            child: TextField(
-              decoration: InputDecoration(hintText: "enter name",border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
+            child: Padding(
+              padding: const EdgeInsets.only(right: 30,left: 30),
+              child: TextField(
+                decoration: InputDecoration(hintText: "enter name",
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
+              ),
             ),
           ),
           SizedBox(
@@ -30,8 +34,12 @@ class _HomePageState extends State<HomePage> {
           ),
           Container(
 
-            child: TextField(
-              decoration: InputDecoration(hintText: "enter email",border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 30,right: 30),
+              child: TextField(
+                decoration: InputDecoration(hintText: "enter email",
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
+              ),
             ),
           ),
           SizedBox(
@@ -39,10 +47,46 @@ class _HomePageState extends State<HomePage> {
           ),
           Container(
 
-            child: TextField(
-              decoration: InputDecoration(hintText: "enter email pasword",border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
+            child: Padding(
+              padding: const EdgeInsets.only(right: 30,left: 30),
+              child: TextField(
+                decoration: InputDecoration(hintText: "enter email pasword",
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
+              ),
             ),
           ),
+          Container(
+            child: FloatingActionButton(onPressed: (){
+              showModalBottomSheet(context: context, builder:(context) {
+                return Container(
+                  height: 200,
+                  width: 90,
+                  color: Colors.green,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30,left: 30,right: 30),
+                        child: TextField(
+                          decoration: InputDecoration(hintText: "enter email pasword",
+                              border: OutlineInputBorder(borderRadius: BorderRadius.circular(20),),
+                          ),
+
+                        ),
+
+                      ),
+                      FloatingActionButton(onPressed: (){
+                        Navigator.pop(context);
+                      },child: Text("sumite"),),
+                    ],
+                  ),
+                );
+
+              },);
+            },
+              child: Text("loging"),
+            )
+
+            ,)
         ],
       ),
     );
